bbac6fd535754147c31083252d52c8cfe9974354
muneeb shah <muneeshah91@gmail.com>
2023-03-24 11:17:34 -0700

    work Accomplish

---
+++ javascript/app.js
@@ -0,0 +1,223 @@
+//{Creator SHAH}//
+"use strict";
+// start javascript {Hello wolrd} //
+
+//start//
+// 1st answer is MADE BY MUNEEB SHAH //
+// alert ("MADE BY MUNEEB SHAH");
+//end//
+
+//start//
+//2nd answer is 4 //
+// var a = (2);
+// var b = (2);
+// alert   (a+b);
+//end//
+
+//start//
+//3rd answer is -3 //
+// var a = (2);
+// var b = (5);
+// alert   (a-b);
+//end//
+
+//start//
+//4th answer is 15 //
+// var a = (3);
+// var b = (5);
+// alert   (a*b);
+//end//
+
+
+//start//
+//5th answer is eror {This is a illegal}  //
+// var 5a = (7);
+// var 7b = (8);
+// alert   (5a*7b);
+//end//
+
+
+//start//
+//6th answer is 15 {This is a legal} //
+// var  $a = (7);
+// var  $b = (8);
+// alert   ($a+$b);
+//end//
+
+
+//start//
+//7th answer is 27 {This is a legal} //
+// var  _a = (11);
+// var  _b = (16);
+// alert   (_a+_b);
+//end//
+
+
+//start//
+//8th answer is 28 {This is a legal} //
+// var  Ja = (11);
+// var  Kb = (17);
+// alert   (Ja+Kb);
+//end//
+
+
+//start//
+//9th answer is MK //
+// var a = ("K");
+// var b = ("M");
+// var c = (b+a)
+// alert   (c);
+//end//
+
+
+//start//
+//10th answer is xyz22 //
+// var a = (22);
+// var b = ("xyz");
+// var c = (b+a)
+// alert   (c);
+//end//
+
+
+//start//
+//11th answer is 46 {console.log} //
+// var a = (2);
+// var b = (44);
+// var c = (a+b)
+// console.log (c)
+//end//
+
+
+//{Creator SHAH}//
+// start javascript {Hello wolrd} //
+
+//start//
+//1st answer is MADE BY MUNEEB SHAH //
+// alert ("MADE BY MUNEEB SHAH");
+//end//
+
+//start//
+//2nd answer is 4 //
+// var a = (2);
+// var b = (2);
+// alert   (a+b);
+//end//
+
+//start//
+//3rd answer is -3 //
+// var a = (2);
+// var b = (5);
+// alert   (a-b);
+//end//
+
+//start//
+//4th answer is 15 //
+// var a = (3);
+// var b = (5);
+// alert   (a*b);
+//end//
+
+
+//start//
+//5th answer is eror {This is a illegal}  //
+// var 5a = (7);
+// var 7b = (8);
+// alert   (5a*7b);
+//end//
+
+
+//start//
+//6th answer is 15 {This is a legal} //
+// var  $a = (7);
+// var  $b = (8);
+// alert   ($a+$b);
+//end//
+
+
+//start//
+//7th answer is 27 {This is a legal} //
+// var  _a = (11);
+// var  _b = (16);
+// alert   (_a+_b);
+//end//
+
+
+//start//
+//8th answer is 28 {This is a legal} //
+// var  Ja = (11);
+// var  Kb = (17);
+// alert   (Ja+Kb);
+//end//
+
+
+//start//
+//9th answer is MK //
+// var a = ("K");
+// var b = ("M");
+// var c = (b+a)
+// alert   (c);
+//end//
+
+
+//start//
+//10th answer is xyz22 //
+// var a = (22);
+// var b = ("xyz");
+// var c = (b+a)
+// alert   (c);
+//end//
+
+
+//start//
+//11th answer is 46 {console.log} //
+// let $a = (2);
+// let $b = (44);
+// let c = ($a+$b)
+// console.log (c)
+//end//
+
+
+//start//
+//INPUT//
+// var yorname = " & Age";
+// var shah = prompt("Enter Your Name" + yorname);
+// document.write(shah)
+//end//
+
+
+//start//
+// post increament//
+// let a = 5;
+// let b = ++a;
+//  console.log(b)
+
+//introduce to let not use var ///
+// let  a = ("boy")
+//  a = ("women")
+//  let b = ("smart boy")
+//  console.log (b)
+//  console.log (a)
+
+//string indexing >>
+// let name = ("shah")
+// S  H  A  H //
+// 1  2  3  4 //
+//  console.log (name[0])
+//ANSER IS (S)
+// console.log (name.length -2)
+// LAST INDEX LENGTH // ANSWER IS 3 //
+
+
+//UPPER CASE LOWER CASE //
+// let name = "muneeb shah";
+// // console.log()
+// alert(name.toUpperCase())
+
+
+//TRIM SPACE//
+// let shah = ("   shah    ")
+// console.log(shah.length)
+//  shah = shah.trim()
+// console.log(shah.length)
+
+
